b96dc076b23e1d0a606bd4307a280613fa0d1bd9
PoH98 <shunyuan98@gmail.com>
2021-05-17 15:39:35 +0800

    整合了HttpResponseMessage

--- src/KaiheilaBot.Plugin/Models/MessageHttps/ResponseMessage/ResponseMessage.cs
+++ src/KaiheilaBot.Plugin/Models/MessageHttps/ResponseMessage/ResponseMessage.cs
@@ -4,15 +4,9 @@ using Newtonsoft.Json.Linq;
 
 namespace KaiheilaBot
 {
-    public class HttpResponseMessage<T> where T : BaseData
+    public class HttpResponseMessage<T>: HttpResponseMessage where T : BaseData
     {
-        public string code { get; set; }
-        public string message { get; set; }
         public T data { get; set; }
-        public override string ToString()
-        {
-            return JObject.FromObject(this).ToString();
-        }
     }
     public class HttpResponseMessage
     {
