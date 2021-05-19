d98b2533c55fbf79f2d8bab69415fe3a001f1b57
Liam Sho <conan_creeper@outlook.com>
2021-05-20 00:10:37 +0800

    添加说明

--- sample/LogEventDataPlugin/LogEventDataPlugin.cs
+++ sample/LogEventDataPlugin/LogEventDataPlugin.cs
@@ -8,7 +8,8 @@ namespace LogEventDataPlugin
 {
     /// <summary>
     /// 本项目引用了一个临时打包 KaiheilaBot.Core 用于测试的 Nuget 包。
-    /// 
+    /// 如果你想要测试此插件，请打包 KaiheilaBot.Core ，版本设置为 1.0.0，
+    /// 将打包好的 Nuget-Package 放在本地 Nuget Feed 路径下
     /// </summary>
     public class LogEventDataPlugin : IPlugin
     {
