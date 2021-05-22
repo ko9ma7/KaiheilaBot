07f89330eb1957f9e76f7668aba135a5953f4496
Liam Sho <conan_creeper@outlook.com>
2021-05-22 20:35:22 +0800

    修改Event反序列化类，区分消息事件和其他事件

--- src/KaiheilaBot.Core/Common/Serializers/EventSerializer.cs
+++ src/KaiheilaBot.Core/Common/Serializers/EventSerializer.cs
@@ -6,17 +6,32 @@ namespace KaiheilaBot.Core.Common.Serializers
     public static class EventSerializer
     {
         /// <summary>
-        /// 反序列化 Event 信令 0 信息字符串
+        /// 反序列化 Event 信令 0, Type 不为 Number 类型的信息字符串
         /// </summary>
         /// <param name="jsonString">Json 字符串</param>
         /// <typeparam name="T">
         /// Event 信令 0 获取的 Record 类型，
-        /// 命名空间 KaiheilaBot.Core.Models.Events.xxxEvents 下
+        /// 命名空间 KaiheilaBot.Core.Models.Events.xxxEvents 下，
+        /// xxx 不为 MessageRelate
         /// </typeparam>
         /// <returns>反序列化得到的 Record 类型 T</returns>
-        public static BaseEvent<T> Deserialize<T>(string jsonString) where T : IBaseEventExtraBody
+        public static BaseEvent<T> DeserializeEvent<T>(string jsonString) where T : IBaseEventExtraBody
         {
             return JsonSerializer.Deserialize<BaseEvent<T>>(jsonString);
+        }
+
+        /// <summary>
+        /// 反序列化 Event 信令 0, Type 为 Number 类型的信息字符串
+        /// </summary>
+        /// <param name="jsonString">Json 字符串</param>
+        /// <typeparam name="T">
+        /// Event 信令 0 获取的 Record 类型，
+        /// 命名空间 KaiheilaBot.Core.Models.Events.MessageRelatedEvents 下，
+        /// </typeparam>
+        /// <returns>反序列化得到的 Record 类型 T</returns>
+        public static BaseMessageEvent<T> DeserializeMessageEvent<T>(string jsonString) where T : IBaseMessageEventDataExtra
+        {
+            return JsonSerializer.Deserialize<BaseMessageEvent<T>>(jsonString);
         }
     }
 }
