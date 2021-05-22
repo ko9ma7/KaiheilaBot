71a1dcc1330c3ce458190f9ff86a58e7a4fa6a01
Liam Sho <conan_creeper@outlook.com>
2021-05-22 21:05:53 +0800

    修复 typo

--- src/KaiheilaBot.Core/Models/Events/ChannelRelatedEvents/PinnedMessageEvent.cs
+++ src/KaiheilaBot.Core/Models/Events/ChannelRelatedEvents/PinnedMessageEvent.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace KaiheilaBot.Core.Models.Events.ChannelRelatedEvents
 {
-    public record PinnedMessage : IBaseEventExtraBody
+    public record PinnedMessageEvent : IBaseEventExtraBody
     {
         [JsonPropertyName("channel_id")]
         public string ChannelId { get; set; }
--- src/KaiheilaBot.Core/Models/Events/ChannelRelatedEvents/UnpinnedMessageEvent.cs
+++ src/KaiheilaBot.Core/Models/Events/ChannelRelatedEvents/UnpinnedMessageEvent.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace KaiheilaBot.Core.Models.Events.ChannelRelatedEvents
 {
-    public record UnpinnedMessage : IBaseEventExtraBody
+    public record UnpinnedMessageEvent : IBaseEventExtraBody
     {
         [JsonPropertyName("channel_id")]
         public string ChannelId { get; set; }
