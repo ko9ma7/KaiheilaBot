9d0cbea4faa04a8e92b5c52ab1c4dc8993dca698
Liam Sho <conan_creeper@outlook.com>
2021-05-23 17:20:51 +0800

    添加 MessageRelatedEvent 枚举

---
+++ src/KaiheilaBot.Core/Models/Events/EnumMessageRelatedEvents.cs
@@ -0,0 +1,12 @@
+namespace KaiheilaBot.Core.Models.Events
+{
+    public enum EnumMessageRelatedEvents
+    {
+        CardMessageEvent,
+        FileMessageEvent,
+        ImageMessageEvent,
+        KmarkdownMessageEvent,
+        TextMessageEvent,
+        VideoMessageEvent
+    }
+}
