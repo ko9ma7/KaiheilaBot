1f19d21ac1657833555529dd442acf7e36fc5f3e
Liam Sho <conan_creeper@outlook.com>
2021-05-27 15:38:37 +0800

    组合所的 Event 枚举

---
+++ src/KaiheilaBot.Core/Models/Events/EnumEvents.cs
@@ -0,0 +1,56 @@
+﻿namespace KaiheilaBot.Core.Models.Events
+{
+    public enum EnumEvents
+    {
+        // Channel Related Events
+        AddedChannelEvent,
+        AddedReactionEvent,
+        DeletedChannelEvent,
+        DeletedMessageEvent,
+        DeletedReactionEvent,
+        PinnedMessageEvent,
+        UnpinnedMessageEvent,
+        UpdatedChannelEvent,
+        UpdatedMessageEvent,
+
+        // Guild Member Events
+        ExitedGuildEvent,
+        GuildMemberOfflineEvent,
+        GuildMemberOnlineEvent,
+        JoinedGuildEvent,
+        UpdatedGuildMemberEvent,
+
+        // Guild Related Events
+        AddedBlockListEvent,
+        DeletedBlockListEvent,
+        DeletedGuildEvent,
+        UpdatedGuildEvent,
+
+        // Guild Role Events
+        AddedRoleEvent,
+        DeletedRoleEvent,
+        UpdatedRoleEvent,
+
+        // Message Related Events
+        CardMessageEvent,
+        FileMessageEvent,
+        ImageMessageEvent,
+        KmarkdownMessageEvent,
+        TextMessageEvent,
+        VideoMessageEvent,
+
+        // Private Message Events
+        DeletedPrivateMessageEvent,
+        PrivateAddedReactionEvent,
+        PrivateDeletedReactionEvent,
+        UpdatedPrivateMessageEvent,
+
+        // User Related Events
+        ExitedChannelEvent,
+        JoinedChannelEvent,
+        MessageBtnClickEvent,
+        SelfExitedGuildEvent,
+        SelfJoinedGuildEvent,
+        UserUpdatedEvent
+    }
+}
