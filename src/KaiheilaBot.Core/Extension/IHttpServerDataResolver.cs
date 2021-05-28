b635231fbd9c4f71174b7555cdfc7d7a0e42d30a
Liam Sho <conan_creeper@outlook.com>
2021-05-28 08:57:39 +0800

    Resolver 添加 ILogger 参数

--- src/KaiheilaBot.Core/Extension/IHttpServerDataResolver.cs
+++ src/KaiheilaBot.Core/Extension/IHttpServerDataResolver.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using KaiheilaBot.Core.Models.Service;
+using KaiheilaBot.Core.Services.IServices;
+using Microsoft.Extensions.Logging;
 
 namespace KaiheilaBot.Core.Extension
 {
     public interface IHttpServerDataResolver
     {
-        public Task Resolve(HttpServerData data);
+        public Task Resolve(HttpServerData data, ILogger<IPlugin> logger, IHttpApiRequestService httpApiRequestService);
     }
 }
